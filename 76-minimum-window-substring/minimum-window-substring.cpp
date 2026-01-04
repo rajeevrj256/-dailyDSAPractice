96c569c990186af6c48f907a26a3494c8688cae8
RAJIV RANJAN <98413843+rajeevrj256@users.noreply.github.com>
2026-01-04 16:12:48 +0530

    Time: 12 ms (44.53%) | Memory: 11.4 MB (80.75%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -3,40 +3,39 @@ public:
     string minWindow(string s, string t) {
         if(t.size()>s.size()) return "";
         unordered_map<char,int>hash;
-        for(char c:t){
-            hash[c]++;
+
+        for(char ch:t){
+            hash[ch]++;
         }
 
-        int count=0;
         int i=0;
+        int n=s.size();
         int j=0;
-        int minLen=INT_MAX;
-        int start=0;
-        while(j<s.size()){
+        int startIndex=0;
+        int maxLen=INT_MAX;
+        int count=0;
+        while(j<n){
+           if(hash[s[j]]>0){
+            count++;
+           }
+           hash[s[j]]--;
 
-            if(hash[s[j]]>0){
-                count++;
-            }
-            hash[s[j]]--;
+           while(count==t.size()){
+             if(j-i+1<maxLen){
+                maxLen=j-i+1;
+                startIndex=i;
+             }
+             hash[s[i]]++;
 
-            while(count==t.size()){
-               if (j - i + 1 < minLen) {
-                    minLen = j - i + 1;
-                    start = i;
-                }
+             if(hash[s[i]]>0) count--;
 
-                 hash[s[i]]++;
-                 if(hash[s[i]]>0)count--;
-                 
-                 i++;
-            }
-            
-            j++;
+             i++;
+           }
+
+           j++;
         }
 
-          return (minLen == INT_MAX) ? "" : s.substr(start, minLen);
-
-
+        return (maxLen==INT_MAX?"":s.substr(startIndex,maxLen));
 
     }
 };
