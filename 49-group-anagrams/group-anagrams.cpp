a95487e528cc9281b7636e9780642fe8f41bbf20
RAJIV RANJAN <98413843+rajeevrj256@users.noreply.github.com>
2026-01-04 18:38:19 +0530

    Time: 67 ms (12.94%) | Memory: 37.5 MB (5.05%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -3,14 +3,21 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string,vector<string>>hash;
 
-        for(auto it:strs){
-            string st=it;
-            sort(it.begin(),it.end());
-            hash[it].push_back(st);
+        for(string str:strs){
+            vector<int>freq(26,0);
+            for(char c:str){
+                freq[c-'a']++;
+            }
+
+            string hashValue;
+
+            for(int i=0;i<26;i++){
+                hashValue+='#'+to_string(freq[i]);
+            }
+            hash[hashValue].push_back(str);
         }
 
         vector<vector<string>>ans;
-
         for(auto it:hash){
             ans.push_back(it.second);
         }
