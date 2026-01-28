7dd08cb69515ae120ca70fad799942498175994e
RAJIV RANJAN <98413843+rajeevrj256@users.noreply.github.com>
2026-01-28 08:45:20 +0530

    Time: 8 ms (48.41%) | Memory: 11.9 MB (86.49%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.cpp
+++ 227-basic-calculator-ii/basic-calculator-ii.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     int calculate(string s) {
-        stack<int>st;
+        long result = 0;
         char last_operator='+';
         int curr=0;
+        int last=0;
 
         for(int i=0;i<s.size();i++){
             char c=s[i];
@@ -14,17 +15,16 @@ public:
 
             if(!isdigit(c) && !isspace(c) || i==s.size()-1){
                 if(last_operator=='+'){
-                      st.push(curr);
+                      result += last;
+                      last=curr;
                 }else if (last_operator=='-'){
-                    st.push(-curr);
+                    result += last;
+                    last=-curr;
                 }else if(last_operator=='*'){
-                    int top=st.top();
-                    st.pop();
-                    st.push(top*curr);
+                   
+                    last=last*curr;
                 }else if(last_operator=='/'){
-                    int top=st.top();
-                    st.pop();
-                    st.push(top/curr);
+                    last=last/curr;
                 }
                 last_operator=c;
                 curr=0;
@@ -33,13 +33,9 @@ public:
             }
         }
 
-        int ans=0;
-        while(!st.empty()){
-           ans+=st.top();
-           st.pop();
-        }
+        
 
 
-        return ans;
+        return result+last;
     }
 };
