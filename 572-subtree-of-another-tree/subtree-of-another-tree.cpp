263f4ff5ae3ff18a191760605574c0a0c12fd422
RAJIV RANJAN <98413843+rajeevrj256@users.noreply.github.com>
2026-02-16 01:20:11 +0530

    Time: 0 ms (100.00%) | Memory: 29 MB (70.39%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.cpp
+++ 572-subtree-of-another-tree/subtree-of-another-tree.cpp
@@ -1,19 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    bool isSame(TreeNode* root, TreeNode* subRoot) {
-        if (root == NULL || subRoot == NULL) {
-            return root == subRoot;
+    bool isSame(TreeNode*root,TreeNode*node){
+        if(root==NULL || node==NULL){
+            return root==node;
         }
 
-        return (root->val == subRoot->val) &&
-               isSame(root->left, subRoot->left) &&
-               isSame(root->right, subRoot->right);
+
+        return root->val==node->val && isSame(root->left,node->left) && isSame(root->right,node->right);
     }
-
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-        if (root == NULL) return false; // If the main tree is empty, it can't contain subRoot.
+        if(root==NULL ) return false;
 
-        // Check if current tree matches subRoot OR check in left subtree OR check in right subtree.
-        return isSame(root, subRoot) || isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
+        return isSame(root,subRoot) || isSubtree(root->left,subRoot) || isSubtree(root->right,subRoot);
     }
 };
