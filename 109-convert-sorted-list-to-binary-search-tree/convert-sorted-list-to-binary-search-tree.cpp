e5b69ba47e1a9f7b5afdb116bc03b03d6f824616
RAJIV RANJAN <98413843+rajeevrj256@users.noreply.github.com>
2026-02-15 17:05:56 +0530

    Time: 8 ms (10.35%) | Memory: 33.1 MB (61.83%) - LeetSync

--- 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.cpp
+++ 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.cpp
@@ -1,45 +1,36 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-     TreeNode* convert(vector<int>& arr, int start, int end) {
-        if (start > end)
-            return NULL;
-        int mid = start + (end - start) / 2;
-        TreeNode* node = new TreeNode(arr[mid]);
-        node->left = convert(arr, start, mid - 1);
-        node->right = convert(arr, mid + 1, end);
+    ListNode* curr;   // global pointer
 
-        return node;
+    int getSize(ListNode* head){
+        int count = 0;
+        while(head){
+            count++;
+            head = head->next;
+        }
+        return count;
+    }
+
+    TreeNode* buildBST(int n){
+        if(n <= 0) return NULL;
+
+        // build left subtree
+        TreeNode* left = buildBST(n/2);
+
+        // current node becomes root
+        TreeNode* root = new TreeNode(curr->val);
+        curr = curr->next;
+
+        // build right subtree
+        root->left = left;
+        root->right = buildBST(n - n/2 - 1);
+
+        return root;
     }
 
     TreeNode* sortedListToBST(ListNode* head) {
-        vector<int> arr;
-        ListNode* temp = head;
-        while (temp) {
-            arr.push_back(temp->val);
-            temp = temp->next;
-        }
-
-        return convert(arr, 0, arr.size() - 1);
+        curr = head;
+        int n = getSize(head);
+        return buildBST(n);
     }
 };
