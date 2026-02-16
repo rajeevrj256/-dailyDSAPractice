4415bf5d2cdd6ff0d7e607126b4772209dd08449
RAJIV RANJAN <98413843+rajeevrj256@users.noreply.github.com>
2026-02-16 13:29:44 +0530

    Time: 2 ms (29.98%) | Memory: 11.7 MB (14.07%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
@@ -1,31 +1,16 @@
 class Solution {
 public:
-    bool check(string str1,string str2){
-        int i=0;
-        int n=str1.size();
-        int j=0;
-        int m=str2.size();
-        if(n%m!=0) return false;
-        for(int i=0;i<n;i++){
-            if(str1[i]!=str2[i%m]) return false;
+    string gcdOfStrings(string str1, string str2) {
+        if(str1.size()<str2.size()){
+            return gcdOfStrings(str2,str1);
         }
 
-        return true;
-    }
-    string gcdOfStrings(string str1, string str2) {
-        string prefix="";
-        string ans="";
-       if(str2.size() > str1.size())
-            swap(str1, str2);
-int i=0;
-          while(i<str2.size()){
-            prefix+=str2[i];
-            if(check(str1,prefix) && check(str2,prefix)){
-                ans=prefix;
-            }
-            i++;
-          }
+        if(str2.empty()){
+            return str1;
+        }
 
-          return ans;
+        if(str1.substr(0,str2.size())!=str2) return "";
+
+        return gcdOfStrings(str1.substr(str2.size()),str2);
     }
 };
