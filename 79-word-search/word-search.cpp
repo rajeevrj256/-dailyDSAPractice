386f4ce2100352dec4619667d920a25d25f3b387
RAJIV RANJAN <98413843+rajeevrj256@users.noreply.github.com>
2026-02-20 15:18:50 +0530

    Time: 730 ms (34.34%) | Memory: 11.7 MB (19.78%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,40 +1,38 @@
 class Solution {
 public:
-    bool dfs(vector<vector<char>>& board, string& word, int x, int y, int index, int n, int m) {
-        if (index >= word.size()) return true;
+   int dx[4]={-1,1,0,0};
+   int dy[4]={0,0,-1,1};
+    bool check(vector<vector<char>>&board,string res,string word,vector<vector<int>>&vis,int x,int y){
+        int n=board.size();
+        int m=board[0].size();
+        
+           if(board[x][y] != word[res.size()])
+            return false;
 
-        // Correct order of boundary check and character match
-        if (x < 0 || x >= n || y < 0 || y >= m || board[x][y] != word[index]) return false;
-
-        char temp = board[x][y];  // Store the original character
-        board[x][y] = '#';  // Mark as visited
-
-        int dx[4] = {-1, 1, 0, 0};
-        int dy[4] = {0, 0, -1, 1};
-
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            if (dfs(board, word, nx, ny, index + 1, n, m)) {
-                return true;
+        vis[x][y]=1;
+        res.push_back(board[x][y]);
+        if(res==word) return true;
+        for(int i=0;i<4;i++){
+            int nx=dx[i]+x;
+            int ny=dy[i]+y;
+            if(nx<n && ny<m && nx>=0 && ny>=0 && !vis[nx][ny]){
+               
+                if(check(board,res,word,vis,nx,ny)) return true;
             }
         }
-
-        board[x][y] = temp;  // Backtrack to restore the original character
+        vis[x][y]=0;
+        res.pop_back();
 
         return false;
     }
-
     bool exist(vector<vector<char>>& board, string word) {
-        int n = board.size();
-        int m = board[0].size();
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (board[i][j] == word[0] && dfs(board, word, i, j, 0, n, m)) {
+         int n=board.size();
+        int m=board[0].size();
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(check(board,"",word,vis,i,j))
                     return true;
-                }
             }
         }
 
