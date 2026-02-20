9f28403e105543ded1b47452913faabeae7bb2b5
RAJIV RANJAN <98413843+rajeevrj256@users.noreply.github.com>
2026-02-20 13:42:09 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (46.53%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,39 +1,34 @@
 class Solution {
 public:
-    bool isValid(vector<string>&board,int row,int col,int n){
-        for(int i=row-1;i>=0;i--){
-            if(board[i][col]=='Q') return false;
-        }
-        for(int i=row-1,j=col+1;i>=0 && j<n;i--,j++ ){
-            if(board[i][j]=='Q') return false;
-        }
-        for(int i=row-1,j=col-1;i>=0 && j>=0;i--,j--){
-            if(board[i][j]=='Q') return false;
-
-        }
-
-
-        return true;
-
-
-    }
-    void backtracking(vector<vector<string>>&ans,vector<string>&res,int row,int n){
+   
+    void backtracking(vector<vector<string>>&ans,vector<string>&res,int row,int n,vector<int>&cola,vector<int>&diga1,vector<int>&diga2){
         if(row>=n){
             ans.push_back(res);
             return;
         }
         for(int col=0;col<n;col++){
-           if(isValid(res,row,col,n)){
+           
+           if(cola[col] || diga1[row-col+n] || diga2[row+col]) continue;
             res[row][col]='Q';
-            backtracking(ans,res,row+1,n);
+            cola[col]=1;
+            diga1[row-col+n]=1;
+            diga2[row+col]=1;
+            backtracking(ans,res,row+1,n,cola,diga1,diga2);
             res[row][col]='.';
+             cola[col]=0;
+            diga1[row-col+n]=0;
+            diga2[row+col]=0;
            }
         }
-    }
+    
+
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>>ans;
         vector<string>res(n,string(n,'.'));
-        backtracking(ans,res,0,n);
+        vector<int>col(n,0);
+        vector<int>diga1(2*n,0);
+        vector<int>diga2(2*n,0);
+        backtracking(ans,res,0,n,col,diga1,diga2);
         return ans;
     }
 };
