7dbb51df12507e0ab4d1dd32410513ea2fde4120
RAJIV RANJAN <98413843+rajeevrj256@users.noreply.github.com>
2026-02-22 17:44:38 +0530

    Time: 15 ms (53.47%) | Memory: 25.7 MB (28.06%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
-    int solve(vector<int>&nums,int target,int index,int sign,int cal){
-       
+    int solve(vector<int>&nums,int target,int index,int cal,vector<vector<int>>&dp,int offset){
+        
         if(index==nums.size()) return (cal==target)?1:0;
+        if(dp[index][cal+offset]!=-1) return dp[index][cal+offset];
+        int plus=solve(nums,target,index+1,nums[index]+cal,dp,offset);
+        int minus=solve(nums,target,index+1,cal-nums[index],dp,offset);
 
-        int plus=solve(nums,target,index+1,0,nums[index]+cal);
-        int minus=solve(nums,target,index+1,0,cal-nums[index]);
-
-        return plus+minus;
+        return dp[index][cal+offset]=plus+minus;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        return solve(nums,target,0,0,0);
+        int n=nums.size();
+        int totalSum=0;
+        for(int num:nums){
+            totalSum+=num;
+        }
+        vector<vector<int>>dp(n,vector<int>(2*totalSum+1,-1));
+        return solve(nums,target,0,0,dp,totalSum);
     }
 };
