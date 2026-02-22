fc8cf58e88d1e64e5bd3461eb2291e76be71d2e2
RAJIV RANJAN <98413843+rajeevrj256@users.noreply.github.com>
2026-02-22 17:38:59 +0530

    Time: 967 ms (5.14%) | Memory: 11.5 MB (91.92%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,49 +1,15 @@
 class Solution {
 public:
-//    int totalsum=0;
-//    int solve(vector<int>&arr,int index,int sum,int target,vector<vector<int>>&dp){
-    
-//     if( index==arr.size()){
-//         return (sum==target?1:0);
-//     }
+    int solve(vector<int>&nums,int target,int index,int sign,int cal){
+       
+        if(index==nums.size()) return (cal==target)?1:0;
 
-//     if(dp[index][sum+totalsum]!=-1)return dp[index][sum+totalsum];
+        int plus=solve(nums,target,index+1,0,nums[index]+cal);
+        int minus=solve(nums,target,index+1,0,cal-nums[index]);
 
-
-
-//     int takeplus=solve(arr,index+1,sum+arr[index],target,dp);
-//     int takemins=solve(arr,index+1,sum-arr[index],target,dp);
-
-
-//     return dp[index][sum+totalsum]=takeplus+takemins;
-
-
-
-    
-
-//    }
+        return plus+minus;
+    }
     int findTargetSumWays(vector<int>& nums, int target) {
-        int n=nums.size();
-        int totalsum=accumulate(nums.begin(),nums.end(),0);
-        if(abs(target)>totalsum) return 0;
-       vector<int>prev(2*totalsum+1,0);
-        
-        prev[nums[0]+totalsum]+=1;
-        prev[-nums[0]+totalsum]+=1;
-
-        for(int i=1;i<n;i++){
-            vector<int>curr(2*totalsum+1,0);
-            for(int j=-totalsum;j<=totalsum;j++){
-                if (prev[j + totalsum] > 0) {
-                curr[j+nums[i]+totalsum]+=prev[j+totalsum];
-                curr[j-nums[i]+totalsum]+=prev[j +totalsum];
-                }
-
-                //dp[i][j+totalsum]=skip+take;
-            }
-            prev=curr;
-        }
-
-        return prev[target+totalsum];
+        return solve(nums,target,0,0,0);
     }
 };
