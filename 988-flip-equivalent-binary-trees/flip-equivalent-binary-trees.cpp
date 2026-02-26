19fe263df894f9c317300818769619560df2ddc4
RAJIV RANJAN <98413843+rajeevrj256@users.noreply.github.com>
2026-02-26 23:03:27 +0530

    Time: 0 ms (100.00%) | Memory: 15.5 MB (11.44%) - LeetSync

--- 988-flip-equivalent-binary-trees/flip-equivalent-binary-trees.cpp
+++ 988-flip-equivalent-binary-trees/flip-equivalent-binary-trees.cpp
@@ -11,36 +11,22 @@
  */
 class Solution {
 public:
-    bool flipEquiv(TreeNode* root1, TreeNode* root2) {
-        if(!root1 && !root2) return true;
-        if(!root1 || !root2) return false;
-        queue<pair<TreeNode*,TreeNode*>>q;
- 
+    bool flipEquiv(TreeNode* r1, TreeNode* r2) {
 
-        q.push({root1,root2});  
+    if(!r1 && !r2) return true;
+    if(!r1 || !r2) return false;
 
-        while(!q.empty() ){
-            int size=q.size();
-            auto [node1,node2]=q.front();
-            q.pop();
-             if(!node1 && !node2) continue;
-        if(!node1 || !node2) return false;
-        if(node1->val!=node2->val) return false;
+    if(r1->val != r2->val)
+        return false;
 
-        TreeNode* a=node1->left;
-         TreeNode* b=node1->right;
-          TreeNode* c=node2->left;
- TreeNode* d=node2->right;
-
- if((a && c && a->val==c->val) || (!a && !c)){
-    q.push({a,c});
-    q.push({b,d});
- }else{
-    q.push({a,d});
-    q.push({b,c});
- }
-        }
-
-        return true;
-    }
+    return (
+        flipEquiv(r1->left, r2->left) &&
+        flipEquiv(r1->right, r2->right)
+    )
+    ||
+    (
+        flipEquiv(r1->left, r2->right) &&
+        flipEquiv(r1->right, r2->left)
+    );
+}
 };
