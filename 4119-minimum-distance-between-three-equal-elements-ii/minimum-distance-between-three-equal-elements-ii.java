117a4f12126469071c58850bddabf3367a5c62e3
RAJIV RANJAN <98413843+rajeevrj256@users.noreply.github.com>
2026-04-11 10:59:46 +0530

    Time: 104 ms (77.65%) | Memory: 275.4 MB (25.14%) - LeetSync

---
+++ 4119-minimum-distance-between-three-equal-elements-ii/minimum-distance-between-three-equal-elements-ii.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int findMiniWindow(List<Integer> index){
+        int i=0;
+        int j=0;
+        int n=index.size();
+        int minDist=Integer.MAX_VALUE;
+        while(j<n){
+            if(j-i+1<3)j++;
+            else if(j-i+1==3){
+                int dist=2*(index.get(j)-index.get(i));
+                minDist=Math.min(minDist,dist);
+                i++;
+                j++;
+            }
+        }
+
+        return minDist;
+    }
+    public int minimumDistance(int[] nums) {
+        Map<Integer,List<Integer>>hash=new HashMap<>();
+
+        for(int i=0;i<nums.length;i++){
+            hash.computeIfAbsent(nums[i],k->new ArrayList<>()).add(i);
+        }
+       
+        int mini = Integer.MAX_VALUE;
+        for(List<Integer>arr:hash.values()){
+            if(arr.size()>=3){
+                mini=Math.min(mini,findMiniWindow(arr));
+            }
+        }
+
+        return mini==Integer.MAX_VALUE?-1:mini;
+    }
+}
